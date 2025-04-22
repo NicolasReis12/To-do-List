50f4aa2a2d729a312302f841764d06f06603d980
NicolasReis12 <seuemail@example.com>
2025-04-22 16:55:25 -0300

    Criação de components

--- to_do_list/src/App.jsx
+++ to_do_list/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
+
 import './App.css'
+
+import Todo from './components/Todo.jsx';
+
 function App() {
   const [todos, setTodos] = useState([
     {
@@ -28,18 +32,7 @@ function App() {
       <h1>Lista de tarefas</h1>
       <div className='todo-list'>
         {todos.map((todo)=>(
-          <div className="todo">
-            <div className="content">
-              <p>{todo.text}</p>
-              <div className="category">
-                <p>({todo.category})</p>
-              </div>
-              <div>
-                <button>Completar</button>
-                <button>Deletar</button>
-              </div>
-            </div>
-          </div>
+          <Todo todo={todo} />
         ))}
       </div>
     </div>
